30deb2540d6cac2e8fa7f577d3ffe0b5c74cbb7e
Todd Trowbridge <84187785+todd-trowbridge@users.noreply.github.com>
2021-09-01 20:52:55 -0400

    merged in add-google-cloud-storage

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+# use python reddit api wrapper
 import praw
+# use google storage
+from google.cloud import storage
 
 # praw setup
 reddit = praw.Reddit(
@@ -12,7 +15,31 @@ reddit = praw.Reddit(
 
 # pull list of subreddits to monitor from firestore
 
+def create_bucket_class_location(bucket_name):
+    """Create a new bucket in specific location with storage class"""
+    # bucket_name = "your-new-bucket-name"
 
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    bucket.storage_class = "COLDLINE"
+    new_bucket = storage_client.create_bucket(bucket, location="us")
+
+    print(
+        "Created bucket {} in {} with storage class {}".format(
+            new_bucket.name, new_bucket.location, new_bucket.storage_class
+        )
+    )
+    return new_bucket
+
+def list_buckets():
+    """Lists all buckets."""
+
+    storage_client = storage.Client()
+    buckets = storage_client.list_buckets()
+
+    for bucket in buckets:
+        print(bucket.name)
 
 for comment in reddit.subreddit("").stream.comments():
     # create custom comment
