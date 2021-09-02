c9ac71b25dea833df3efe5ac2b0791403e019229
Todd Trowbridge <84187785+todd-trowbridge@users.noreply.github.com>
2021-09-01 21:51:48 -0400

    added test functions

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 import praw
 # use google storage
 from google.cloud import storage
+# use io to create virtual files without saving
+import io
 
 # praw setup
 reddit = praw.Reddit(
@@ -15,15 +17,14 @@ reddit = praw.Reddit(
 
 # pull list of subreddits to monitor from firestore
 
-def create_bucket_class_location(bucket_name):
+def add_subreddit_bucket(bucket_name):
     """Create a new bucket in specific location with storage class"""
-    # bucket_name = "your-new-bucket-name"
 
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
-    bucket.storage_class = "COLDLINE"
-    new_bucket = storage_client.create_bucket(bucket, location="us")
+    bucket.storage_class = "STANDARD"
+    new_bucket = storage_client.create_bucket(bucket, location="US-EAST1")
 
     print(
         "Created bucket {} in {} with storage class {}".format(
@@ -34,14 +35,46 @@ def create_bucket_class_location(bucket_name):
 
 def list_buckets():
     """Lists all buckets."""
-
+    bucket_list = []
     storage_client = storage.Client()
     buckets = storage_client.list_buckets()
 
     for bucket in buckets:
-        print(bucket.name)
+        bucket_list.append(bucket.name)
+    return bucket_list
 
-list_buckets()
+def check_save_subreddit_bucket(subreddit_id):
+    subredditId = subreddit_id
+    bucket_list = list_buckets()
+    try:
+        bucket_list.index(subreddit_id)
+        print("bucket {} already exists".format(subredditId))
+    except:
+        print("creating new bucket for {}".format(subredditId))
+        add_subreddit_bucket(subreddit_id)
+    return ""
+
+# test
+check_save_subreddit_bucket("t5_123456")
+
+def upload_blob(json_as_string, bucket_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_string(json_as_string)
+
+#test
+upload_blob('{{"hello": "world"}}', "t5_123456", "TEST BLOB")
+
+
 
 # for comment in reddit.subreddit("").stream.comments():
 #     # create custom comment
